4ec786d23c6bee16efee257bee12340e0a6bb9f6
Daniel Tschan <tschan@puzzle.ch>
2016-03-19 21:25:52 +0100

    Save certificate to file.

--- sign.sh
+++ sign.sh
@@ -4,7 +4,7 @@ domain="$1"
 
 cd /var/lib/letsencrypt
 
-[ -e ${domain}.key ] || openssl genrsa 4096 > ${domain}.key
-[ -e ${domain}.csr ] || openssl req -new -sha256 -key ${domain}.key -subj "/CN=${domain}" > ${domain}.csr
+[ -s ${domain}.key ] || openssl genrsa 4096 > ${domain}.key
+[ -s ${domain}.csr ] || openssl req -new -sha256 -key ${domain}.key -subj "/CN=${domain}" > ${domain}.csr
 
-python /usr/local/bin/acme_tiny.py --account-key account.key --csr ${domain}.csr --acme-dir /srv/.well-known/acme-challenge/
+python /usr/local/bin/acme_tiny.py --account-key account.key --csr ${domain}.csr --acme-dir /srv/.well-known/acme-challenge/ >${domain}.crt
