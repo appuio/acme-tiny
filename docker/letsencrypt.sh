42cd9a984034eb049f7662c5361824ece72d3d2e
Daniel Tschan <tschan@puzzle.ch>
2016-04-08 16:11:04 +0200

    Add more output.

--- docker/letsencrypt.sh
+++ docker/letsencrypt.sh
@@ -11,8 +11,16 @@ cd /var/lib/letsencrypt
 [ -s ${domain}.csr ] || openssl req -new -sha256 -key ${domain}.key -subj "/CN=${domain}" > ${domain}.csr
 
 if ! [ -s ${domain}.crt ] || ! openssl x509 -checkend 2592000 -noout -in ${domain}.crt; then
-  # No valid cert or cert will expire within the next 30 days, renew
+  if [ -s ${domain.crt} ]; then
+    echo "Renewing certificate for ${domain}"
+  else
+    echo "Creating certificate for ${domain}"
+  fi
+
   python /usr/local/letsencrypt/acme_tiny.py --account-key account.key --csr ${domain}.csr --acme-dir /srv/.well-known/acme-challenge/ >${domain}.crt
+else
+  echo "We already have a certificate for ${domain} which is still valid for at least 30 days."
 fi
 
+echo "Configuring certificate for requests to https://${domain}/"
 /usr/local/letsencrypt/insert-certificate.sh -h $domain -c ${domain}.crt -k ${domain}.key
